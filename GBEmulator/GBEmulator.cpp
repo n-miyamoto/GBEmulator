3d181adb5d310921c8c938c8362e43263f68ce41
n-miyamoto <scotsdowneroad@gmail.com>
2020-03-26 23:58:24 +0900

    chande gldrawpixels to 2dtexture

--- GBEmulator/GBEmulator.cpp
+++ GBEmulator/GBEmulator.cpp
@@ -5,10 +5,23 @@
 #include "Gameboy.h"
 #pragma warning(disable: 4996) 
 
-#define IMAGE_SIZE_IN_BYTE (4 * FRAME_WIDTH * FRAME_HEIGHT)
+#define IMAGE_SIZE_IN_BYTE (3 * FRAME_WIDTH * FRAME_HEIGHT)
+
+#ifdef EMSCRIPTEN
+    #include <emscripten/emscripten.h>
+    #define GL_GLEXT_PROTOTYPES
+    #define EGL_EGLEXT_PROTOTYPES
+#endif
 
 unsigned char* bitmap;
+unsigned char* dest;
 Gameboy* GB;
+
+const int modifier = 10;
+ 
+// Window size
+int display_width = FRAME_WIDTH * modifier;
+int display_height = FRAME_HEIGHT * modifier;
 
 //Create bitmap
 void create_bitmap(unsigned char* bitmap) {
@@ -16,12 +29,11 @@ void create_bitmap(unsigned char* bitmap) {
 
 	for (int i = 0; i < FRAME_HEIGHT; i++) {
 		for (int j = 0; j < FRAME_WIDTH; j++) {
-			int offset = (FRAME_HEIGHT-i-1) * FRAME_WIDTH + j;
+			int offset = i * FRAME_WIDTH + j;
 			val = 255 - GB->gpu.frame_buffer[i*FRAME_WIDTH+j]*64;
-			bitmap[offset * 4 + 0] = val;
-			bitmap[offset * 4 + 1] = val;
-			bitmap[offset * 4 + 2] = val;
-			bitmap[offset * 4 + 3] = 255;
+			bitmap[offset * 3 + 0] = val;
+			bitmap[offset * 3 + 1] = val;
+			bitmap[offset * 3 + 2] = val;
 		}
 	}
 }
@@ -67,10 +79,30 @@ void key_release(unsigned char key , int x , int y) {
 
 //Rasterize callback
 static void draw() {
-  create_bitmap(bitmap);
-  glClear(GL_COLOR_BUFFER_BIT);
-  glDrawPixels(FRAME_WIDTH, FRAME_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, bitmap);
-  glFlush();
+	create_bitmap(bitmap);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glTexSubImage2D(GL_TEXTURE_2D, 0 ,0, 0, FRAME_WIDTH, FRAME_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)bitmap);	
+    glBegin( GL_QUADS );
+        glTexCoord2d(0.0, 0.0);		glVertex2d(0.0,			  0.0);
+        glTexCoord2d(1.0, 0.0); 	glVertex2d(display_width, 0.0);
+        glTexCoord2d(1.0, 1.0); 	glVertex2d(display_width, display_height);
+        glTexCoord2d(0.0, 1.0); 	glVertex2d(0.0,			  display_height);
+    glEnd();
+
+	glutSwapBuffers();  
+}
+
+void reshape_window(GLsizei w, GLsizei h)
+{
+    glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, w, h, 0);        
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, w, h);
+ 
+    display_width = w;
+    display_height = h;
 }
 
 //Timer Callback
@@ -123,26 +155,38 @@ int main(int argc, char *argv[])
 	fread(bootrom, sizeof(uint8_t), boot_rom_size, br);
 	fclose(br);
 
-
 	//init GameBoy
 	Gameboy gb(rom, rom_size, bootrom);
 	gb.showCartInfo();
 	GB = &gb;
 
 	bitmap = new unsigned char[IMAGE_SIZE_IN_BYTE];
+	dest = new unsigned char[IMAGE_SIZE_IN_BYTE];
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA);
 	glutInitWindowSize(FRAME_WIDTH, FRAME_HEIGHT);
+	glutInitWindowPosition(320, 320);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA );
 	glutCreateWindow("bitmap");
 	glutDisplayFunc(draw);
 	glutIgnoreKeyRepeat(GL_TRUE);	
 	glutIdleFunc(idle);
+	glutReshapeFunc(reshape_window);
 	glutTimerFunc(100, timer, 0);
 	glutKeyboardFunc(key_press);
 	glutKeyboardUpFunc(key_release);
 
+	glTexImage2D(
+		GL_TEXTURE_2D, 0, 3, FRAME_WIDTH, FRAME_HEIGHT,
+		0, GL_RGB, GL_UNSIGNED_BYTE, bitmap
+	);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP); 
+	glEnable(GL_TEXTURE_2D);
 	glutMainLoop();
+
 
 	free(rom);
 	delete[] bitmap;
